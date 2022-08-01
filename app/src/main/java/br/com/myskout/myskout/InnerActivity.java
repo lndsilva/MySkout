d79f6af70bbfb09bd4fffe428b87c3b7efa85e23
lndsilva <lndsilva@hotmail.com>
2022-08-01 10:32:10 -0300

    toast mensagem

--- app/src/main/java/br/com/myskout/myskout/InnerActivity.java
+++ app/src/main/java/br/com/myskout/myskout/InnerActivity.java
@@ -4,20 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InnerActivity extends AppCompatActivity {
+    //Declarar as variáveis que irão receber o xml
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //abrir o meu layout
         setContentView(R.layout.inner_layout);
+        //Apresentar as variaveis Java para as variaveis xml
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        //Criando a ação no botão entrar
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String usuario, senha;
+
+                usuario = edtUsuario.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                //Validar a entrada do usuário
+
+                if (usuario.equals("Senac") && senha.equals("Senac")) {
+
+                }
+                else {
+                   //Enviando uma caixa com mensagem para o usuário
+
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+
+
+
+
+                }
+
+
+            }
+        });
+
+
     }
 
+
+    //Vai sair do sistema
     public void sairSistema(View view) {
         finish();
     }
-
-
-
 
 }
--- app/src/main/java/br/com/myskout/myskout/SplashActivity.java
+++ app/src/main/java/br/com/myskout/myskout/SplashActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
     }
 }
