427c4fe41b05b51e8c862d09f2a52ba51814780f
lndsilva <lndsilva@hotmail.com>
2022-08-05 09:17:57 -0300

    criando fab para cadastrar parcerias

--- app/src/main/java/br/com/myskout/myskout/ParceriasActivity.java
+++ app/src/main/java/br/com/myskout/myskout/ParceriasActivity.java
@@ -1,8 +1,13 @@
 package br.com.myskout.myskout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ParceriasActivity extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class ParceriasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parcerias_layout);
+    }
+
+    //Criando e adicionando o menu na activity parcerias
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Instanciando o menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_parcerias, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Criando os cliques nos itens de menu
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mParcAlterar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei em Alterar",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mParcExcluir:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei em Excluir",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mParcPesquisar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei em Pesquisar",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mParcSair:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei em Sair",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
