e858d773873a589336ada33d30b2ab68831ccda7
lndsilva <lndsilva@hotmail.com>
2022-08-08 10:31:49 -0300

    criando dados para preencher

--- app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
+++ app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
@@ -1,4 +1,4 @@
-package br.com.myskout.myskout;
+'package br.com.myskout.myskout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +11,31 @@ import android.widget.ListView;
 public class RecrutadoresActivity extends AppCompatActivity {
     //Declarando variavel global para carregar a lista
     ListView listaRecrutadores;
+    //Base de dados para inserir no ListView
+
+    int imgRecrutadores[] = {
+            R.drawable.recrutador,
+    };
+    String tiluloRecrutadores[] = {"Kleiton José",};
+    String cargoRecrutadores[] = {"Gerente de Recursos", "Consultor de recursos humanos",
+            "Gestor de recrutamento e seleção", "Assistente de departamento de pessoal",
+            "Supervisor de cargos", "Analista de cargos", "Analista de treinamento",
+            "Analista de recursos humanos", "Diretor de recursos humanos"};
+    String descricaoRecrutadores[] = {"Trabalha no recrutamento de pessoas a mais de 10 anos, é especialista de desenvolvimento de soft skills e hard skils.",
+            "Gerencia pessoas e contratação de pessoas",
+            "Realiza análise e gestão de processos de recursos humanos",
+            "Realiza processos de contratação de pessoas em diversos cargos",
+            "Auxiliar na análise de documentação e curriculos",
+            "Supervisão dos processos de pessoas","Verifica os cargos e salários",
+            "Realiza a criação de treinamento de pessoas e processos."
+
+
+    };
+    String rating[] = {"4,5",};
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recrutadores_layout);
@@ -29,7 +51,7 @@ public class RecrutadoresActivity extends AppCompatActivity {
     }
 
     //Criando uma innerclass - Classe dentro da classe
-    public class AdaptadorRecr extends BaseAdapter{
+    public class AdaptadorRecr extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -46,10 +68,11 @@ public class RecrutadoresActivity extends AppCompatActivity {
             return 0;
         }
 
-        //Que será implementado o modelo da lista
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
+
+
             return null;
         }
     }
-}
+}'
