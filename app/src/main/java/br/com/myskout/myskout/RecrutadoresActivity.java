8e9bda3dba83f26cea2ee4f9d1a6a85a9cf3f020
lndsilva <lndsilva@hotmail.com>
2022-08-08 08:37:56 -0300

    implementando listView

--- app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
+++ app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
@@ -3,12 +3,24 @@ package br.com.myskout.myskout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RecrutadoresActivity extends AppCompatActivity {
+    //Declarando variavel global para carregar a lista
+    ListView listaRecrutares;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recrutadores_layout);
+
+        //Informando o xml para o java
+        listaRecrutares = findViewById(R.id.lstRecrutadores);
+
+        //Instanciando o Adaptador
+
+        //estou carregando os itens na lista
+        listaRecrutares.setAdapter();
     }
 }
