f838af2065efd1dcef8810b58de1fcc674bfde5f
lndsilva <lndsilva@hotmail.com>
2022-08-08 08:48:17 -0300

    criando a classe baseAdapter

--- app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
+++ app/src/main/java/br/com/myskout/myskout/RecrutadoresActivity.java
@@ -3,11 +3,14 @@ package br.com.myskout.myskout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class RecrutadoresActivity extends AppCompatActivity {
     //Declarando variavel global para carregar a lista
-    ListView listaRecrutares;
+    ListView listaRecrutadores;
 
 
     @Override
@@ -16,11 +19,37 @@ public class RecrutadoresActivity extends AppCompatActivity {
         setContentView(R.layout.recrutadores_layout);
 
         //Informando o xml para o java
-        listaRecrutares = findViewById(R.id.lstRecrutadores);
+        listaRecrutadores = findViewById(R.id.lstRecrutadores);
 
         //Instanciando o Adaptador
+        AdaptadorRecr adapter = new AdaptadorRecr();
 
         //estou carregando os itens na lista
-        listaRecrutares.setAdapter();
+        listaRecrutadores.setAdapter(adapter);
+    }
+
+    //Criando uma innerclass - Classe dentro da classe
+    public class AdaptadorRecr extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        //Que será implementado o modelo da lista
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
