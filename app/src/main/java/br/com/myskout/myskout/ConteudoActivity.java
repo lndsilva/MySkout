9ee3e9a108650f4b2a6f5f56674aa4bc028d699a
lndsilva <lndsilva@hotmail.com>
2022-08-10 09:57:04 -0300

    inserindo ações no conteudo

--- app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
+++ app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ConteudoActivity extends AppCompatActivity {
+    //Declarando variável global para o componente
+    BottomNavigationView bottomNavigationView;
+
+    //Instanciar todos os fragments que serão utilizados neste layout
+    TalentosFragment talentosFragment = new TalentosFragment();
+    VagasFragment vagasFragment = new VagasFragment();
+    RecrutadoresFragment recrutadoresFragment = new RecrutadoresFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conteudo_layout);
+
+        bottomNavigationView = findViewById(R.id.idBottomNavView);
+
+        
+
     }
 }
