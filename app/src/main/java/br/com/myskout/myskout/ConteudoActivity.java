872b6df9e19b0ec550f3c3946dd77a073f23e893
lndsilva <lndsilva@hotmail.com>
2022-08-10 10:31:12 -0300

    inserindo ações no conteudo

--- app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
+++ app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
@@ -1,10 +1,13 @@
 package br.com.myskout.myskout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ConteudoActivity extends AppCompatActivity {
     //Declarando variável global para o componente
@@ -22,7 +25,41 @@ public class ConteudoActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.idBottomNavView);
 
-        
+        //Carregando a janela default no container
+        getSupportFragmentManager().
+                beginTransaction().
+                replace(R.id.idContainer, talentosFragment)
+                .commit();
+
+        //Inserindo evento no bottomNavView
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.mTalentos:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.idContainer, talentosFragment)
+                                .commit();
+                        return true;
+                    case R.id.mRecrutadores:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.idContainer, recrutadoresFragment)
+                                .commit();
+                        return true;
+                    case R.id.mVagas:
+                        getSupportFragmentManager().
+                                beginTransaction().
+                                replace(R.id.idContainer, vagasFragment)
+                                .commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
 
     }
 }
