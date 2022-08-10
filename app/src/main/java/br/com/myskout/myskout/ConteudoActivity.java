e6920d393a4b6726a33374e3d279972b3242746d
lndsilva <lndsilva@hotmail.com>
2022-08-10 08:19:14 -0300

    bottom navigation e fragments

---
+++ app/src/main/java/br/com/myskout/myskout/ConteudoActivity.java
@@ -0,0 +1,14 @@
+package br.com.myskout.myskout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConteudoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conteudo_layout);
+    }
+}
