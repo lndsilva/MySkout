6df5607a0632ac2556ee215f1b1bc3b66dc39aff
lndsilva <lndsilva@hotmail.com>
2022-08-10 09:26:04 -0300

    inserindo ações do fragment

--- app/src/main/java/br/com/myskout/myskout/TalentosActivity.java
+++ app/src/main/java/br/com/myskout/myskout/TalentosActivity.java
@@ -3,12 +3,18 @@ package br.com.myskout.myskout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 
 public class TalentosActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.talentos_layout);
+
+
     }
 }
